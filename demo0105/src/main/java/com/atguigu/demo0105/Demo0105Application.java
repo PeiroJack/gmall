bf0a3af1d6850118b902924ca9099ea7c4c1cf4e
PeiroJack <3532926353@qq.com>
2020-03-23 19:36:34 +0800

    提交测试

---
+++ demo0105/src/main/java/com/atguigu/demo0105/Demo0105Application.java
@@ -0,0 +1,13 @@
+package com.atguigu.demo0105;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo0105Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Demo0105Application.class, args);
+    }
+
+}
---
+++ demo0105/src/test/java/com/atguigu/demo0105/Demo0105ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atguigu.demo0105;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demo0105ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
