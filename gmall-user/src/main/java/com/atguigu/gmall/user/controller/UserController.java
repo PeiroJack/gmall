9ca1851c774dc89615a5e1e67b8d91dc84cacf46
PeiroJack <3532926353@qq.com>
2020-03-24 21:18:21 +0800

    学习使用通用mapper

--- gmall-user/src/main/java/com/atguigu/gmall/user/controller/UserController.java
+++ gmall-user/src/main/java/com/atguigu/gmall/user/controller/UserController.java
@@ -1,9 +1,12 @@
 package com.atguigu.gmall.user.controller;
 
 import com.atguigu.gmall.user.bean.UmsMember;
+import com.atguigu.gmall.user.bean.UmsMemberReceiveAddress;
 import com.atguigu.gmall.user.service.UserService;
+import org.apache.ibatis.annotations.Param;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -15,7 +18,14 @@ public class UserController {
     @Autowired
     UserService userService;
 
-    @RequestMapping("getAllUser")
+    @RequestMapping("getReceiveAddressByMemberId")
+    @ResponseBody
+    public List<UmsMemberReceiveAddress> getReceiveAddressByMemberId(String memberId){
+        List<UmsMemberReceiveAddress> umsMemberReceiveAddresses = userService.getReceiveAddressByMemberId(memberId);
+        return umsMemberReceiveAddresses;
+    }
+	
+	@RequestMapping("getAllUser")
     @ResponseBody
     public List<UmsMember> getAllUser(){
         List<UmsMember> umsMembers = userService.getAllUser();
